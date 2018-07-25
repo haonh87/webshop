a6b89603135b7d7ed66a6fc93441bb90ae2dcc71
Tinh Ha Khac <tinhhak@HarveyNash.vn.local>
2018-07-25 14:05:07 +0700

    update validate

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,67 +6,18 @@
     </div>
     <div class="row">
         <div class="col-md-12">
-            <form action="{{ action('Admin\CustomerController@store') }}" method="POST">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <form action="{{ action('Admin\CustomerController@store') }}" class='form-validate' method="POST">
+                <div class="row">
+                    <div class="col col-md-6">
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
+                        </div>
+                    </div>
+                    <div class="col col-md-6">
 
-                <table class="table">
-                    <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>
-                            <input type="text" name = "name" value="" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Address</td>
-                        <td>
-                            <input type="text" name = "address" value="" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Email</td>
-                        <td>
-                            <input type="text" name = "email" value="" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Phone</td>
-                        <td>
-                            <input type="text" name = "phone" value="" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Mobile</td>
-                        <td>
-                            <input type="text" name = "mobile" value="" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Gender</td>
-                        <td>
-                              <select class="form-control" name="gender" style="width: 24% !important">
-                                <option value ="0">Male</option>
-                                <option value ="1">Female</option>
-                              </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Status</td>
-                        <td>
-                          <select class="form-control" name="status" style="width: 24% !important">
-                            <option value ="0">Single</option>
-                            <option value ="1">Maried</option>
-                          </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Birth day</td>
-                        <td>
-                          <input type="text" placeholder="dd/mm/yyyy" required name="DOB">
-                        </td>
-                    </tr>
-                    </tbody>
-                  </table>
+                    </div>
+                </div>
 
                 <a class="btn btn-default" href="{{ route('admin.user.index') }}">Back</a>
                 <button class="btn btn-primary" type="submit" >Create</button>
--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -55,93 +55,14 @@
         <b class="copyright">&copy; 2014 Edmin - EGrappler.com </b>All rights reserved.
     </div>
 </div>
-{{--<div id="wrapper">--}}
-
-{{--<!-- Navigation -->--}}
-{{--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">--}}
-{{--<!-- Brand and toggle get grouped for better mobile display -->--}}
-{{--<div class="navbar-header">--}}
-{{--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">--}}
-{{--<span class="sr-only">Toggle navigation</span>--}}
-{{--<span class="icon-bar"></span>--}}
-{{--<span class="icon-bar"></span>--}}
-{{--<span class="icon-bar"></span>--}}
-{{--</button>--}}
-{{--<a class="navbar-brand" href="{{route('adminIndex')}}">MPMODA Admin</a>--}}
-{{--</div>--}}
-{{--<!-- Top Menu Items -->--}}
-{{--<ul class="nav navbar-right top-nav">--}}
-{{--<li class="dropdown">--}}
-{{--<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>--}}
-{{--{{ Auth::user()->name }}--}}
-{{--<b class="caret"></b></a>--}}
-{{--<ul class="dropdown-menu">--}}
-{{--<li>--}}
-{{--<a href="{{ route('logout') }}"><i class="fa fa-fw fa-user"></i> Logout</a>--}}
-{{--</li>--}}
-{{--</ul>--}}
-{{--</li>--}}
-{{--</ul>--}}
-{{--<div class="collapse navbar-collapse navbar-ex1-collapse">--}}
-{{--<ul class="nav navbar-nav side-nav">--}}
-{{--<li>--}}
-{{--<a href="--}}{{-- {{ route('admin.products.index') }} --}}{{--"><i class="fa fa-fw fa-dashboard"></i>Product--}}
-{{--Management</a>--}}
-{{--</li>--}}
-{{--<li>--}}
-{{--<a href="{{ action('Admin\CategoryController@index') }}"><i class="fa fa-fw fa-dashboard"></i>Category--}}
-{{--Management</a>--}}
-{{--</li>--}}
-{{--<li>--}}
-{{--<a href="{{ action('Admin\VoteController@index') }}"><i class="fa fa-fw fa-dashboard"></i>Vote--}}
-{{--Management</a>--}}
-{{--</li>--}}
-{{--<li>--}}
-{{--<a href="{{ action('Admin\OrderController@index') }}">--}}
-{{--<i class="fa fa-fw fa-dashboard"></i>Order Management</a>--}}
-{{--</li>--}}
-{{--@if(Auth::user()->role_id ==1)--}}
-{{--<li>--}}
-{{--<a href="{{ action('Admin\UserController@index') }}"><i class="fa fa-fw fa-dashboard"></i>Account--}}
-{{--Management</a>--}}
-{{--</li>--}}
-{{--<li>--}}
-{{--<a href="{{ action('Admin\CustomerController@index') }}"><i class="fa fa-fw fa-dashboard"></i>Customer--}}
-{{--Management</a>--}}
-{{--</li>--}}
-{{--@endif--}}
-{{--</ul>--}}
-{{--</div>--}}
-{{--<!-- /.navbar-collapse -->--}}
-{{--</nav>--}}
-
-{{--<div id="page-wrapper">--}}
-
-{{--<div class="container-fluid">--}}
-
-{{--<!-- Page Heading -->--}}
-{{--<div class="row">--}}
-{{--<div class="col-lg-12">--}}
-{{--@yield('content')--}}
-{{--</div>--}}
-{{--</div>--}}
-{{--<!-- /.row -->--}}
-
-{{--</div>--}}
-{{--<!-- /.container-fluid -->--}}
-
-{{--</div>--}}
-{{--<!-- /#page-wrapper -->--}}
-
-{{--</div>--}}
-<!-- /#wrapper -->
-
-<!-- jQuery -->
 <script src="{{ asset('admins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('admins/scripts/flot/jquery.flot.js') }}" type="text/javascript"></script>
 <script src="{{ asset('admins/scripts/flot/jquery.flot.resize.js') }}" type="text/javascript"></script>
 <script src="{{ asset('admins/scripts/datatables/jquery.dataTables.js') }}" type="text/javascript"></script>
+{{--to file not load--}}
+<script src='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/js/bootstrapvalidator.min.js'></script>
+{{--to file not load--}}
 <script src="{{ asset('admins/scripts/common.js') }}" type="text/javascript"></script>
-@yield('script_close')
+@yield('script')
 </body>
 </html>
