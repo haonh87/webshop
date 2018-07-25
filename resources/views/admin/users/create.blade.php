6f684943102ce237517bb2629569495b2f28f08e
Tinh Ha Khac <tinhhak@HarveyNash.vn.local>
2018-07-25 15:39:43 +0700

    update

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -7,17 +7,33 @@
     <div class="row">
         <div class="col-md-12">
             <form action="{{ action('Admin\CustomerController@store') }}" class='form-validate' method="POST">
+
                 <div class="row">
                     <div class="col col-md-6">
-                        <div class="form-group-custom">
+                        <div class="form-group">
                             <label for="email">Email address</label>
                             <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
                         </div>
                     </div>
                     <div class="col col-md-6">
-                        <div class="form-group-custom">
+                        <div class="form-group">
                             <label for="Name">Name</label>
                             <input type="text" class="form-control" name="name" id="name" placeholder="Enter name">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col col-md-6">
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
+                        </div>
+                    </div>
+                    <div class="col col-md-6">
+                        <div class="form-group">
+                            <label for="Name">Gender</label>
+                            <input type="text" class="form-control" name="gender" id="gender" placeholder="Enter gender">
                         </div>
                     </div>
                 </div>
