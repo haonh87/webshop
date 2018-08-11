de7308afbe49d70cb0326f02360017c3072bc3aa
hong_hanu <lehong03011996@gmail.com>
2018-08-11 11:16:16 +0700

    update sidebar for post management

--- resources/views/layouts/admin/widgets/sidebar.blade.php
+++ resources/views/layouts/admin/widgets/sidebar.blade.php
@@ -15,15 +15,10 @@
         </li>
         {{--Post Management--}}
         <li>
-            <a class="collapsed" data-toggle="collapse" href="#productManagement"><i class="menu-icon icon-bullhorn"></i>Quản lý bài viết</a>
-            <ul id="productManagement" class="collapse list-unstyled">
-                <li><a href="{{ route('admin.category-management') }}"><i class="icon-inbox"></i>Danh mục sản phẩm</a></li>
-                <li><a href="{{ route('products.index') }}"><i class="icon-inbox"></i>Danh sách sản phẩm</a></li>
-                <li><a href="{{ route('admin.product.color-management') }}"><i class="icon-inbox"></i>Màu sắc sản phẩm</a></li>
-                <li><a href="{{ route('admin.product.size-management') }}"><i class="icon-inbox"></i>Kích cỡ sản phẩm</a></li>
+            <a class="collapsed" data-toggle="collapse" href="#postManagement"><i class="menu-icon icon-bullhorn"></i>Quản lý bài viết</a>
+            <ul id="postManagement" class="collapse list-unstyled">
                 <li><a href="{{ route('admin.post-category-management') }}"><i class="icon-inbox"></i>Danh mục bài viết</a></li>
                 <li><a href="{{ route('admin.post-management') }}"><i class="icon-inbox"></i>Bài viết</a></li>
-                <li><a href="other-user-listing.html"><i class="icon-inbox"></i>Thêm mới sản phẩm</a></li>
             </ul>
         </li>
         {{--user manager--}}
