153d85847bae864ce69caf435457f2e5da5e67e4
Tinh Ha Khac <tinhhak@HarveyNash.vn.local>
2018-08-15 10:12:47 +0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,28 +2,34 @@
     <div class="bottom_bg">
         <div class="bottom_outer">
             <div class="bottom_inner sidebar_layout_14141414">
-                <aside id="text-3" class="widget widget_text"><h3 class="widgettitle">Information</h3>
+                <aside id="text-3" class="widget widget_text">
+                    <h3 class="widgettitle">Liên Hệ</h3>
                     <div class="textwidget">
                         <ul class="styled_list">
-                            <li><a href="/">How to make an order?</a></li>
-                            <li><a href="/">About Us</a></li>
+                            <li><a href="#"><i class="fa fa-map-marker-alt"></i>196 Cầu Giấy, Hà Nội, Việt Nam</a></li>
+                            <li><a href="#">(028) 540 12341</a></li>
+                            <li><a href="#">0914 777 332</a></li>
+                            <li><a href="#">thecity@rongphuongbac.com</a></li>
                         </ul>
                     </div>
                 </aside>
-                <aside id="text-5" class="widget widget_text"><h3 class="widgettitle">Customer Service</h3>
+                <aside id="text-3" class="widget widget_text">
+                    <h3 class="widgettitle">Giới Thiệu</h3>
                     <div class="textwidget">
                         <ul class="styled_list">
-                            <li>
-                                <a href="/">Contact Us</a></li>
+                            <li><a href="/">Giới thiệu</a></li>
+                            <li><a href="/">Hướng dẫn mua hàng</a></li>
                         </ul>
                     </div>
                 </aside>
-                <aside id="text-4" class="widget widget_text"><h3 class="widgettitle">My Account</h3>
+                <aside id="text-5" class="widget widget_text">
+                    <h3 class="widgettitle">Tài khoản</h3>
                     <div class="textwidget">
                         <ul class="styled_list">
-                            <li><a href="/">My  Account </a></li>
-                            <hr/>
-                            <li><a href="/">Wish List</a></li>
+                            <li><a href="{{ route('myaccount.index') }}">Đăng nhập</a></li>
+                            <li><a href="{{ route('logout') }}">Đăng xuất</a></li>
+                            <li><a href="{{ route('myaccount.create') }}">Tạo tài khoản mới</a></li>
+                            <li><a href="{{ route('myaccount.edit') }}">Sửa thông tin tài khoản</a></li>
                         </ul>
                     </div>
                 </aside>
