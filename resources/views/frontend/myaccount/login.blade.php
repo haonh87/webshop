9933407c9c3ea31a0b981b57fc93f33b3c4f88a8
Tinh Ha Khac <tinhhak@HarveyNash.vn.local>
2018-08-15 10:21:12 +0700

    update login

--- resources/views/frontend/myaccount/login.blade.php
+++ resources/views/frontend/myaccount/login.blade.php
@@ -13,31 +13,31 @@
                             <div class="cmsmasters_column_inner">
                                 <div class="cmsmasters_text">
                                     <div class="woocommerce">
-                                        <h2>Login</h2>
+                                        <h2>Đăng Nhập</h2>
                                         <form action="{{ route('login') }}" class="woocommerce-form woocommerce-form-login login" method="post">
                                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                                <label for="username">Email address&nbsp;<span
+                                                <label for="username">Nhập email<span
                                                             class="required">*</span></label>
                                                 <input type="text"
                                                        class="woocommerce-Input woocommerce-Input--text input-text"
                                                        name="email" id="email" autocomplete="email" value="">
                                             </p>
                                             <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                                                <label for="password">Password&nbsp;<span
+                                                <label for="password">Mật khẩu&nbsp;<span
                                                             class="required">*</span></label>
                                                 <input class="woocommerce-Input woocommerce-Input--text input-text"
                                                        type="password" name="password" id="password"
-                                                       autocomplete="current-password">
+                                                       autocomplete="current-password" style="width: 260px">
                                             </p>
                                             <p class="form-row">
                                                 <button type="submit" class="woocommerce-Button button" name="login"
-                                                        value="Log in">Log in
+                                                        value="Log in">Đăng Nhập
                                                 </button>
                                                 <label class="woocommerce-form__label woocommerce-form__label-for-checkbox inline">
                                                     <input class="woocommerce-form__input woocommerce-form__input-checkbox"
                                                            name="rememberme" type="checkbox" id="rememberme"
-                                                           value="forever"> <span>Remember me</span>
+                                                           value="forever"> <span>Nhớ đăng nhập</span>
                                                 </label>
                                             </p>
                                         </form>
