deeb3758d6b6b684da5e5db1690e7a3279408026
hong_hanu <lehong03011996@gmail.com>
2018-08-19 15:54:03 +0700

    done product list

--- resources/views/frontend/post/post_detail.blade.php
+++ resources/views/frontend/post/post_detail.blade.php
@@ -12,22 +12,24 @@
                     <article id="post-85"
                              class="cmsmasters_open_post post-85 post type-post status-publish format-image has-post-thumbnail hentry category-advice category-training post_format-post-format-image">
                         <div class="cmsmasters_post_date_wrap"><span class="cmsmasters_post_date"><abbr
-                                        class="published" title="{{$postDetail->created_at}}">{{$postDetail->created_at}}</abbr><abbr
-                                        class="dn date updated" title="{{$postDetail->updated_at}}">{{$postDetail->updated_at}}</abbr></span>
+                                        class="published"
+                                        title="{{$postDetail->created_at}}">{{$postDetail->created_at}}</abbr><abbr
+                                        class="dn date updated"
+                                        title="{{$postDetail->updated_at}}">{{$postDetail->updated_at}}</abbr></span>
                             <header class="cmsmasters_post_header entry-header"><h2
                                         class="cmsmasters_post_title entry-title">{{$postDetail->title}}</h2>
                             </header>
                             <figure class="cmsmasters_img_wrap"><a
                                         title="{{$postDetail->title}}" rel="ilightbox[img_85_5b77b295587aa]"
                                         class="cmsmasters_img_link"><img width="860" height="526"
-                                                                         src="{{$postDetail->image_url}}"
+                                                                         src="{{ asset($postDetail->image_url) }}"
                                                                          class="" alt="6 Full-Body Stretching Exercises"
                                                                          title="{{$postDetail->title}}"
-                                                                         srcset="{{$postDetail->image_url}} 860w, {{$postDetail->image_url}} 300w, {{$postDetail->image_url}} 768w, {{$postDetail->image_url}} 1024w, h{{$postDetail->image_url}} 600w, {{$postDetail->image_url}} 580w,{{$postDetail->image_url}} 1160w"
+                                                                         srcset="{{ asset($postDetail->image_url) }} 860w, {{ asset($postDetail->image_url) }} 300w, {{ asset($postDetail->image_url) }} 768w, {{ asset($postDetail->image_url) }} 1024w, {{ asset($postDetail->image_url) }} 600w, {{ asset($postDetail->image_url) }} 580w,{{ asset($postDetail->image_url) }} 1160w"
                                                                          sizes="(max-width: 860px) 100vw, 860px"/></a>
                             </figure>
                             <div class="cmsmasters_post_content entry-content">
-                                {{$postDetail->content}}
+                                {!!html_entity_decode(@$postDetail->content) !!}
                             </div>
                     </article>
 
@@ -47,32 +49,34 @@
                             <div class="cmsmasters_tab tab_latest">
                                 <ul>
                                     @foreach($newestPost as $post)
-                                    <li>
-                                        <div class="cmsmasters_lpr_tabs_img">
-                                            <figure class="cmsmasters_img_wrap"><a
-                                                        href="{{route('post.show', ['id' => $post->id])}}"
-                                                        title=" {{$post->title}}"
-                                                        class="cmsmasters_img_link"><img width="70" height="70"
-                                                                                         src=" {{$post->image_url}}"
-                                                                                         class=" wp-post-image"
-                                                                                         alt=" {{$post->title}}"
-                                                                                         title=" {{$post->title}}"
-                                                                                         srcset="{{$post->image_url}} 70w,{{$post->image_url}} 150w, {{$post->image_url}} 300w, {{$post->image_url}} 360w,{{$post->image_url}} 580w, {{$post->image_url}} 180w, {{$post->image_url}} 600w"
-                                                                                         sizes="(max-width: 70px) 100vw, 70px"/></a>
-                                            </figure>
-                                        </div>
+                                        <li>
+                                            <div class="cmsmasters_lpr_tabs_img">
+                                                <figure class="cmsmasters_img_wrap"><a
+                                                            href="{{route('post.show', ['id' => $post->id])}}"
+                                                            title=" {{$post->title}}"
+                                                            class="cmsmasters_img_link">
+                                                        <img width="70" height="70"
+                                                             src="{{ asset($post->image_url) }}"
+                                                             class=" wp-post-image"
+                                                             alt=" {{$post->title}}"
+                                                             title=" {{$post->title}}"
+                                                             srcset="{{ asset($post->image_url) }} 70w,{{ asset($post->image_url) }} 150w, {{ asset($post->image_url) }} 300w, {{ asset($post->image_url) }} 360w,{{ asset($post->image_url) }} 580w, {{ asset($post->image_url) }} 180w, {{ asset($post->image_url) }} 600w"
+                                                             sizes="(max-width: 70px) 100vw, 70px"/></a>
+                                                </figure>
+                                            </div>
 
-                                        <div class="cmsmasters_lpr_tabs_cont">
-                                            <a href="{{route('post.show', ['id' => $post->id])}}"
-                                               title="{{$post->title}}">
-                                                {{$post->title}}
-                                            </a>
-                                            <abbr class="published" title="{{$post->created_at}}">{{$post->created_at}}
-                                            </abbr>
-                                        </div>
+                                            <div class="cmsmasters_lpr_tabs_cont">
+                                                <a href="{{route('post.show', ['id' => $post->id])}}"
+                                                   title="{{$post->title}}">
+                                                    {{$post->title}}
+                                                </a>
+                                                <abbr class="published"
+                                                      title="{{$post->created_at}}">{{$post->created_at}}
+                                                </abbr>
+                                            </div>
 
-                                    </li>
-                                        @endforeach
+                                        </li>
+                                    @endforeach
                                 </ul>
                             </div>
                         </div>
--- resources/views/frontend/post/post_list.blade.php
+++ resources/views/frontend/post/post_list.blade.php
@@ -13,18 +13,22 @@
 
 </style>
 @section('content')
-    <div id="content">
-        @include('frontend.header_line', ['name' => 'Bài viết'])
-        @include('frontend.message')
+    <div id="middle">
+    @include('frontend.header_line', ['name' => 'Bài viết'])
+    @include('frontend.message')
+    <!--  Start Middle  -->
         <div class="middle_inner">
             <div class="content_wrap fullwidth">
+
+                <!--  Start Content  -->
+                <div class="middle_content entry"></div>
             </div>
             <div id="cmsmasters_row_dd69359c02"
                  class="cmsmasters_row cmsmasters_color_scheme_default cmsmasters_row_top_default cmsmasters_row_bot_default cmsmasters_row_boxed">
                 <div class="cmsmasters_row_outer_parent">
                     <div class="cmsmasters_row_outer">
                         <div class="cmsmasters_row_inner">
-                            <div class="cmsmasters_row_margin cmsmasters_11">
+                            <div class="cmsmasters_row_margin">
                                 <div id="cmsmasters_column_cc9f84b14e" class="cmsmasters_column one_first">
                                     <div class="cmsmasters_column_inner">
                                         <div id="blog_fb6a93754e" class="cmsmasters_wrap_blog entry-summary"
@@ -33,27 +37,24 @@
                                              data-orderby="date" data-order="ASC" data-count="6"
                                              data-categories="advice,fitness-fashion,perfect-body,sports-together,training"
                                              data-metadata="date,more" data-pagination="pagination">
-                                            <div class="blog columns grid cmsmasters_3 isotope"
-                                                 style="position: relative; height: 864.876px;">
+                                            <div class="blog columns grid cmsmasters_3">
                                                 <!--  Start Post Masonry Article  -->
 
                                                 @foreach($postList as $key => $post)
                                                     <article id="post-{{$post->id}}"
                                                              class="cmsmasters_post_masonry post-10736 post type-post status-publish format-image has-post-thumbnail hentry category-perfect-body post_format-post-format-image shortcode_animated"
-                                                             data-category="perfect-body"
-                                                             style="width: 325px; position: absolute; left: 0px; top: 0px;">
+                                                             data-category="perfect-body">
                                                         <div class="cmsmasters_post_cont">
                                                             <figure class="cmsmasters_img_wrap">
                                                                 <a href="{{route('post.show', ['id' => $post->id])}}" title="{{$post->title}}"
-                                                                   rel="ilightbox[img_10736_5b77914d5f921]"
                                                                    class="cmsmasters_img_link">
                                                                     <img width="580" height="355"
-                                                                         src="{{$post->image_url}}"
+                                                                         src="{{ asset($post->image_url) }}"
                                                                          class="wp-post-image"
                                                                          alt="{{$post->title}}"
                                                                          title="{{$post->title}}"
                                                                          sizes="(max-width: 580px) 100vw, 580px"
-                                                                         srcset="{{$post->image_url}} 580w, {{$post->image_url}} 300w, {{$post->image_url}} 768w, {{$post->image_url}} 1024w,{{$post->image_url}} 600w, {{$post->image_url}} 860w, {{$post->image_url}} 1160w">
+                                                                         srcset="{{ asset($post->image_url) }} 580w, {{ asset($post->image_url) }} 300w, {{ asset($post->image_url) }} 768w, {{ asset($post->image_url) }} 1024w,{{ asset($post->image_url) }} 600w, {{ asset($post->image_url) }} 860w, {{ asset($post->image_url) }} 1160w">
                                                                 </a>
                                                             </figure>
                                                             <div class="cmsmasters_post_date_wrap"><span
@@ -65,11 +66,11 @@
                                                             </div>
                                                             <header class="cmsmasters_post_header entry-header"><h5
                                                                         class="cmsmasters_post_title entry-title"><a
-                                                                            href="">"{{$post->title}}" </a></h5>
+                                                                            href="{{route('post.show', ['id' => $post->id])}}" title="{{$post->title}}">"{{$post->title}}" </a></h5>
                                                             </header>
                                                             <div class="cmsmasters_post_content entry-content">
                                                                 <p>
-                                                                    {{$post->description}}
+                                                                    {!!html_entity_decode(@$post->description) !!}
                                                                 </p>
                                                             </div>
                                                             <footer class="cmsmasters_post_footer"><a
@@ -83,12 +84,20 @@
                                             </div>
                                             <div class="cmsmasters_wrap_more_posts cmsmasters_wrap_more_items"></div>
                                         </div>
+
                                     </div>
                                 </div>
                             </div>
+                            @include('frontend.elements.paginator', ['paginator' => $postList])
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div class="cl"></div>
+            <div class="content_wrap fullwidth">
+                <div class="middle_content entry"></div>
+                <!--  Finish Content  -->
             </div>
         </div>
     </div>
